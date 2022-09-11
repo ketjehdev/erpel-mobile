75610198e95f4290db3a0cebddaa5a2f7b448dcc
ketjehdev <ketjeh.developer@gmail.com>
2022-09-11 12:58:57 +0700

    test fetch api

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ActivityIndicator,
+  FlatList,
+  usState,
+} from "react-native";
+
+const movieApi = "https://reactnative.dev/movies.json";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [title, setTitle] = useState([]);
+  const [data, setData] = useState([]);
+
+  // fetching API data
+  useEffect(() => {
+    fetch(movieApi)
+      .then((response) => response.json()) // if api give response executed code bottom
+      .then((json) => {
+        setTitle(json.title);
+        setData(json.movies);
+      })
+      .catch((error) => alert(error))
+      .finally(() => setLoading(false));
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>
+              {item.id}, {item.title}
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
 }
